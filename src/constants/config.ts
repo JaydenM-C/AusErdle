b6c65d5e1184b238c34aa0e67f696ac90ac57edf
JaydenM-C <jaydenshere@gmail.com>
2022-02-05 01:50:40 +0100

    Update config.ts

--- src/constants/config.ts
+++ src/constants/config.ts
@@ -1,11 +1,11 @@
 export const CONFIG = {
   tries: 6, // This changes how many tries you get to finish the wordle
-  language: 'YourLanguageHere', // This changes the display name for your language
+  language: 'Australian English phonemic transcription (HCE)', // This changes the display name for your language
   wordLength: 5, // This sets how long each word is based on how many characters (as defined in orthography.ts) are in each word
-  author: 'YourNameHere', // Put your name here so people know who made this Wordle!
-  authorWebsite: 'YourLinkHere', // Put a link to your website or social media here
-  wordListSource: 'YourDictionarySource', // Describe the source material for your words here
-  wordListSourceLink: 'YourDictionaryLink', // Put a link to the source material for your words here
+  author: 'Jayden Macklin-Cordes', // Put your name here so people know who made this Wordle!
+  authorWebsite: 'www.macklincordes.com', // Put a link to your website or social media here
+  wordListSource: 'Wordlist source is the BEEP Pronunciation Dictionary, converted into Australian English phonetic transcription (Harrington, Cox and Evans system) by Jayden Macklin-Cordes using R software. The BNC Frequency lists were also used to filter posssible answers.', // Describe the source material for your words here
+  wordListSourceLink: 'https://www.openslr.org/14/', // Put a link to the source material for your words here
   //
   // THESE NEXT SETTINGS ARE FOR ADVANCED USERS
   //
